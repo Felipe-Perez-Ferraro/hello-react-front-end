886a657a77ed276b782a8a53063862aa2beee6a0
Felipe <fperezferraro@gmail.com>
2023-12-05 13:35:52 -0300

    Added the store provider and the Greeting component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import './App.css';
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Greeting from './components/Greeting';
+import store from './redux/store';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <>
+          <Greeting />
+        </>
+      ),
+    },
+  ]);
+
   return (
-    <div className="App">
-      hola
-    </div>
+    <>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </>
   );
 }
 
